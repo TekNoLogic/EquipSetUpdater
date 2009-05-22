9ed42927074d8ea38882cccaf0f024d7553d4889
Tekkub <tekkub@gmail.com>
2009-05-22 13:29:16 -0600

    And so it begins...

---
+++ EquipSetUpdater.lua
@@ -0,0 +1,87 @@
+
+------------------------------
+--      Util Functions      --
+------------------------------
+
+local function Print(...) print("|cFF33FF99EquipSetUpdater|r:", ...) end
+
+local debugf = tekDebug and tekDebug:GetFrame("EquipSetUpdater")
+local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
+
+
+-----------------------------
+--      Event Handler      --
+-----------------------------
+
+local f = CreateFrame("frame")
+f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
+f:RegisterEvent("ADDON_LOADED")
+
+
+function f:ADDON_LOADED(event, addon)
+	if addon:lower() ~= "equipsetupdater" then return end
+
+	GearManagerDialogSaveSet:SetWidth(80)
+	GearManagerDialogDeleteSet:SetWidth(80)
+
+	local butt = CreateFrame("Button", nil, GearManagerDialog)
+	butt:SetPoint("TOPLEFT", GearManagerDialogDeleteSet, "TOPRIGHT", 1, 0)
+	butt:SetPoint("BOTTOMRIGHT", GearManagerDialogSaveSet, "BOTTOMLEFT", -1, 0)
+
+	butt:SetDisabledFontObject(GameFontDisable)
+	butt:SetHighlightFontObject(GameFontHighlight)
+	butt:SetNormalFontObject(GameFontNormal)
+
+	butt:SetNormalTexture("Interface\\Buttons\\UI-Panel-Button-Up")
+	butt:SetPushedTexture("Interface\\Buttons\\UI-Panel-Button-Down")
+	butt:SetHighlightTexture("Interface\\Buttons\\UI-Panel-Button-Highlight")
+	butt:SetDisabledTexture("Interface\\Buttons\\UI-Panel-Button-Disabled")
+	butt:GetNormalTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+	butt:GetPushedTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+	butt:GetHighlightTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+	butt:GetDisabledTexture():SetTexCoord(0, 0.625, 0, 0.6875)
+	butt:GetHighlightTexture():SetBlendMode("ADD")
+
+	butt:SetText("Update")
+	butt:Disable()
+	self.butt = butt
+
+	butt:SetScript("OnShow", butt.Disable)
+	butt:SetScript("OnClick", self.UpdateSet)
+	hooksecurefunc("GearSetButton_OnClick", function(self)
+		if GearManagerDialog.selectedSet then butt:Enable() else butt:Disable() end
+	end)
+
+	self:RegisterEvent("EQUIPMENT_SETS_CHANGED")
+
+	LibStub("tekKonfig-AboutPanel").new(nil, "EquipSetUpdater")
+
+	self:UnregisterEvent("ADDON_LOADED")
+	self.ADDON_LOADED = nil
+end
+
+
+function f:EQUIPMENT_SETS_CHANGED()
+	self.butt:Disable()
+end
+
+
+local function GetTextureIndex(tex)
+	tex = tex:lower()
+	local numicons = GetNumMacroIcons()
+	for i=INVSLOT_FIRST_EQUIPPED,INVSLOT_LAST_EQUIPPED do if GetInventoryItemTexture("player", i) then numicons = numicons + 1 end end
+	for i=1,numicons do
+		local texture, index = GetEquipmentSetIconInfo(i)
+		if texture:lower() == tex then return index end
+	end
+end
+
+
+function f:UpdateSet()
+	local set = GearManagerDialog.selectedSet
+	if not set then return end
+
+	local texi = GetTextureIndex(set.icon:GetTexture())
+	if not texi then return Print("Error finding icon index, cannot save set.") end
+	SaveEquipmentSet(set.name, texi)
+end
